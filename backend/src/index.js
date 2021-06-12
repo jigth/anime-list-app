c291653a2f1ce7e7d028860eb495f8cea8cb1c5b
jigth <>
2021-06-12 11:17:29 -0500

    Fixing deletion error in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = require('./app');
 
 require('dotenv').config();
-
+const PORT = process.env.PORT || 4450;
 
 async function startServer() {
     await app.listen(PORT);
